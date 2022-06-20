92da903461b142f337fce557bef6734b633309af
moolnam <moolnam>
2022-06-20 10:29:36 +0900

    Initial Commit

---
+++ Realm-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Realm-Test
+//
+//  Created by KimJongHee on 2022/06/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Realm-Test/Realm_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Realm_TestApp.swift
+//  Realm-Test
+//
+//  Created by KimJongHee on 2022/06/20.
+//
+
+import SwiftUI
+
+@main
+struct Realm_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
